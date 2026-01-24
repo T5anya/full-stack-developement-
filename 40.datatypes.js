41434e3be07dc754d757a6767319b50e91fae0e8
tanya <tanyaagr229@gmail.com>
2026-01-24 20:59:10 +0530

    Update 40.datatypes.js

--- 40.datatypes.js
+++ 40.datatypes.js
@@ -1,1 +1,36 @@
-//primitive
+//primitive aisi saari values jinko copy karne par tumhe ek real copy mil jayege 
+//ex- string,number,Boolean,null,bigint, undefined,symbol
+
+//reference-inko copy kadne par real.copy nahi milegi but aapko reference milega parent ka
+//ex-aarays,objects,functions
+
+let a =12;
+let b=a;
+a=a+2;
+ // a=14 b=12
+ 
+let c=[1,2,3];
+let d=c;
+d.pop();
+// a=[1,2]  b=[1,2]
+
+
+//null ka matlab hat jaan bojh kar koi value nahi di 
+//undefined ka matlab hai apne ek variable aur use koi value nahi di uski
+//by default value jogi undefined
+
+let e=9007199254740991n;
+console.log(e+1n);// 9007199254740992
+// the max value a variable can hold is 9007199254740991
+//then for number bigger than this we use bigint
+
+
+//Dynamic typing -js main static typing nahi hai yahn par dynamic typing hai 
+//aap yahan pe data ko change kar sakte hai
+
+int f=12;
+f=true;//possible in js only 
+
+
+//Type Coercion - jismai aapka ek type dusre type mai convert ho jata hai 
+
